7be080422b4739fb182e9df0b57b6a5370dda1ce
sriram <sriram65raja@gmail.com>
2024-05-16 15:27:40 +0530

    Initial commit

---
+++ frontned/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Chats from './Chats'
+
+const App = () => {
+  return (
+    <>
+     <div className='App'>
+      <Chats/>
+     </div>
+    </>
+  )
+}
+
+export default App
